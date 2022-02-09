1273688567d396d87cc079a4638a191dbe349d14
Jafar Abbadi <JafarA@leading-point.com>
2022-02-09 11:50:36 +0200

    fix models

--- src/composer/models/product.model.ts
+++ src/composer/models/product.model.ts
@@ -1,3 +1,4 @@
+import { Category } from "./category.model";
 import { Variant } from "./variant.model";
 
 export interface Product { 
@@ -6,5 +7,7 @@ export interface Product {
     lastUpdateDate: Date;
     name: string;
     description: string;
+    type: 'restuarant'| 'service' | 'electronic' | 'digital' | 'shop'
     variants: Variant[];
+    category: string;
 }
